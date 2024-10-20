1fdb04ddf02d6cf07f3a65470296aeef9af8ce46
Himal Shrestha <shresthahimal12@yahoo.com>
2024-10-19 22:54:02 -0400

    First Commit - Himal Shrestha Commit Homework Module 05

---
+++ app/src/main/java/com/example/quizapp_module5/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.quizapp_module5
+
+/**
+ * Himal Shrestha
+ * Prof. Alrajab
+ * Farmingdale State College
+ * BCS 371 - Mobile Application Development
+ * Homework Module 05 - Quiz App
+ */
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.quizapp_module5.ui.theme.Quizapp_module5Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Quizapp_module5Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Quizapp_module5Theme {
+        Greeting("Android")
+    }
+}
